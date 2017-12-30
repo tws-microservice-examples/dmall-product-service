52df2b68ae3f1c6baa4ff0deaf8df56b2f4ce4f9
jtong <tj19832@gmail.com>
2017-12-31 01:07:00 +0800

    add a new api for order, and try to be called failed

--- src/main/java/com/dmall/product/adapter/ProductController.java
+++ src/main/java/com/dmall/product/adapter/ProductController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/products")
@@ -23,6 +26,16 @@ public class ProductController {
 
   }
 
+  @RequestMapping(value = "", method = RequestMethod.GET, headers = "Accept=application/json")
+  public List<ProductForClient> findBySkus(@RequestParam("skus") String[] skus) {
+    List<Product> products = productRepository.findBySkuIn(Arrays.asList(skus));
+
+    List<ProductForClient> result = products.stream()
+            .map(product -> productApp.convertProductForClients(product))
+            .collect(Collectors.toList());
+    return result;
+  }
+
   @RequestMapping(value = "/{sku}", method = RequestMethod.GET, headers = "Accept=application/json")
   public ProductForClient getProductBySku(@PathVariable("sku") String sku) {
     Product product = productRepository.findBySku(sku);
@@ -36,5 +49,6 @@ public class ProductController {
     Product savedProduct = productRepository.save(product);
     return savedProduct;
   }
+
 
 }
--- src/main/java/com/dmall/product/adapter/ProductRepository.java
+++ src/main/java/com/dmall/product/adapter/ProductRepository.java
@@ -3,6 +3,10 @@ package com.dmall.product.adapter;
 import com.dmall.product.adapter.application.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
     Product findBySku(String sku);
+
+    List<Product> findBySkuIn(List<String> skuList);
 }
