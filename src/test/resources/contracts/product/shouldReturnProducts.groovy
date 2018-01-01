828a70ca4e421f594db6205d7fa47830fee44be1
linwenjun <linwenjun@163.com>
2018-01-02 01:50:20 +0800

    add contract

---
+++ src/test/resources/contracts/product/shouldReturnProducts.groovy
@@ -0,0 +1,42 @@
+package product
+
+import org.springframework.cloud.contract.spec.Contract
+
+[
+        Contract.make {
+            name('should return product')
+            request {
+                method GET()
+                url ('products') {
+                    queryParameters {
+                        parameter('skus', '6009907')
+                        parameter('skus', '6009901')
+                        parameter('skus', '600990')
+                    }
+                }
+            }
+            response {
+                status(200)
+                body """
+                    [
+                        {
+                            "sku": "6009907",
+                            "title": "Apple - iPhone X 64GB - Silver (Verizon)",
+                            "spec": "64GB - Silver (Verizon)",
+                            "brand": "apple",
+                            "pic": "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6009/6009887_sd.jpg;maxHeight=640;maxWidth=550",
+                            "price": 799.99
+                        },
+                        {
+                            "sku": "6009901",
+                            "title": "Apple - iPhone X 64GB - Space Gray (Verizon)",
+                            "spec": "64GB - Space Gray (Verizon)",
+                            "brand": "apple",
+                            "pic": "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6009/6009887_sd.jpg;maxHeight=640;maxWidth=550",
+                            "price": 799.99
+                        }
+                    ]
+                """
+            }
+        }
+]
